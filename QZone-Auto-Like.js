6d2d2465dcca84a8ed7009d49f5b2d04489f96b6
wzh190015 <734649192@qq.com>
2016-08-11 19:20:40 +0800

    function

--- QZone-Auto-Like.js
+++ QZone-Auto-Like.js
@@ -2,37 +2,33 @@
 // @name        QQ空间自动点赞
 // @namespace   firefox
 // @include     http://user.qzone.qq.com
-// @version     1.3
-// @run-at document-end
+// @version     1.4
+// @grant       unsafeWindow
 // @description QQ空间自动点赞,6秒刷新一次
 // ==/UserScript==
 /**
  * 刷新间隔
  */
 var INTERVAL = 6000;
-
 /**
  * 点赞并刷新
  */
-function like()
-{
-	/**
+var like = function () {
+  /**
 	 * 点赞
 	 */
-	jQuery("a.qz_like_btn_v3[data-clicklog='like']").each(function(index,item){
-		console.log(item);
-		jQuery(item).trigger('click');
-	});
-	
-	/**
+  jQuery('a.qz_like_btn_v3[data-clicklog=\'like\']').each(function (index, item) {
+    console.log(item);
+    jQuery(item).trigger('click');
+  });
+  /**
 	 * 刷新
 	 */
-	jQuery("#feed_tab_all").each(function(index,item){
-			jQuery(item).trigger('click');
-		});
-}
-
-/**
+  jQuery('#feed_tab_all').each(function (index, item) {
+    jQuery(item).trigger('click');
+  });
+} /**
  * 以刷新间隔点赞并刷新
  */
-setInterval("like()",INTERVAL);
+
+setInterval('like()', INTERVAL);
